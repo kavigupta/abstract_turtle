3a9edac93f9bb5552d1cfd0095e3fe10eb754b32
Kavi Gupta <kavi@globalmbox.com>
2019-12-30 21:30:02 -0800

    add basic test

---
+++ tests/image_tests.py
@@ -0,0 +1,34 @@
+
+import unittest
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from abstract_turtle.turtle import Turtle
+from abstract_turtle.pillow_canvas import PillowCanvas
+
+# set this to true to create the images
+REFRESH_IMAGES = False
+
+class BasicSquareTest(unittest.TestCase):
+
+    def setUp(self):
+        self.canvas = PillowCanvas(1000, 1000)
+        self.turtle = Turtle(self.canvas)
+
+    def assertImageEquals(self, path):
+        data = self.canvas.export().astype(np.float32) / 255
+        if REFRESH_IMAGES:
+            plt.imsave(path, data)
+        else:
+            saved_data = plt.imread(path)
+            self.assertTrue(np.array_equal(data, saved_data))
+
+    def test_square(self):
+        t = self.turtle
+        for _ in range(36):
+            for _ in range(4):
+                t.fd(100)
+                t.rt(90)
+            t.rt(10)
+        self.assertImageEquals("img/squareflower.png")
