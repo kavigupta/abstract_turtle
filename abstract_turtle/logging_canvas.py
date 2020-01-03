70ee09e58a43f8d26e5eb31313375495a425b20a
Kavi Gupta <kavi@globalmbox.com>
2020-01-03 11:53:46 -0800

    make logging canvas configurable

--- abstract_turtle/logging_canvas.py
+++ abstract_turtle/logging_canvas.py
@@ -1,22 +1,31 @@
 
+from abc import abstractmethod
+
 from .canvas import Canvas
 
-class LoggingCanvas(Canvas):
+class AbstractLoggingCanvas(Canvas):
+    @abstractmethod
+    def on_action(self, log_line):
+        pass
+
+    def draw_rectangular_line(self, start, end, color, width):
+        self.on_action(['draw_rectangular_line', [start.x, start.y, end.x, end.y], color, width])
+
+    def draw_circle(self, center, radius, color, width, is_filled):
+        self.on_action(['draw_circle', [center.x, center.y, radius], color, width, is_filled])
+
+    def fill_polygon(self, points, color):
+        self.on_action(['fill_polygon', [[point.x, point.y] for point in points], color])
+
+    def set_bgcolor(self, color):
+        self.on_action(['set_bgcolor', color])
+
+    def clear(self):
+        self.on_action(['clear'])
+
+class LoggingCanvas(AbstractLoggingCanvas):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.log = []
-
-    def draw_rectangular_line(self, start, end, color, width):
-        self.log.append(['draw_rectangular_line', [start.x, start.y, end.x, end.y], color, width])
-
-    def draw_circle(self, center, radius, color, width, is_filled):
-        self.log.append(['draw_circle', [center.x, center.y, radius], color, width, is_filled])
-
-    def fill_polygon(self, points, color):
-        self.log.append(['fill_polygon', [[point.x, point.y] for point in points], color])
-
-    def set_bgcolor(self, color):
-        self.log.append(['set_bgcolor', color])
-
-    def clear(self):
-        self.log.append(['clear'])
+    def on_action(self, log_line):
+        self.log.append(log_line)
