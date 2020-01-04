49715b82abbd3f8982a11ba214e9be80d6421d8e
Kavi Gupta <kavi@globalmbox.com>
2020-01-04 14:02:03 -0800

    minor refactoring

--- abstract_turtle/model.py
+++ abstract_turtle/model.py
@@ -3,16 +3,14 @@ from collections import namedtuple
 
 from math import sin, cos
 
-def cons_color(*color):
-    if len(color) == 3:
-        if any(not isinstance(c, int) for c in color):
-            raise RuntimeError("Not a valid color: %s" % color)
-        return Color(*color)
-    raise RuntimeError("String colors not supported")
-
-
-Color = namedtuple('Color', ['red', 'green', 'blue'])
-Color.of = cons_color
+class Color(namedtuple('Color', ['red', 'green', 'blue'])):
+    @staticmethod
+    def of(*color):
+        if len(color) == 3:
+            if any(not isinstance(c, int) for c in color):
+                raise RuntimeError("Not a valid color: %s" % color)
+            return Color(*color)
+        raise RuntimeError("String colors not supported")
 
 Position = namedtuple('Position', ['x', 'y'])
 
