f231eb007ff5200f28159aa3e677ea07fbca27ca
rahularya <rahularya@berkeley.edu>
2020-01-07 02:50:16 +0800

    added color strings

--- abstract_turtle/model.py
+++ abstract_turtle/model.py
@@ -3,6 +3,9 @@ from collections import namedtuple
 
 from math import sin, cos
 
+from .color_names import COLORS
+
+
 class Color(namedtuple('Color', ['red', 'green', 'blue'])):
     @staticmethod
     def of(*color):
@@ -10,7 +13,28 @@ class Color(namedtuple('Color', ['red', 'green', 'blue'])):
             if any(not isinstance(c, int) for c in color):
                 raise RuntimeError("Not a valid color: %s" % color)
             return Color(*color)
-        raise RuntimeError("String colors not supported")
+        if len(color) == 1 and isinstance(color[0], str):
+            color = color[0].lower()
+            if color and color[0] == "#":
+                if len(color) == 4:  # shorthand hex
+                    color = "".join(x * 2 for x in color)[1:]
+                if len(color) == 7:
+                    digits = color[1:]
+                    vals = [Color.hexparse(digits[i:i+2]) for i in range(0, 6, 2)]
+                    return Color.of(*vals)
+                raise RuntimeError("Invalid hex color string")
+            if color in COLORS:
+                return Color.of(COLORS[color])
+            raise RuntimeError("Invalid color string")
+        raise RuntimeError("Invalid color")
+
+    @staticmethod
+    def hexparse(pair):
+        try:
+            return int(pair, 16)
+        except ValueError:
+            raise RuntimeError("Invalid hex color string")
+
 
 Position = namedtuple('Position', ['x', 'y'])
 
