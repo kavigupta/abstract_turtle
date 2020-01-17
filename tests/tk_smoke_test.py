7cb6123e0d63535e098016051946049cc6daba56
Kavi Gupta <kavi@globalmbox.com>
2020-01-17 02:40:13 -0800

    add tk smoke test and see if it works on travis??

--- tests/tk_smoke_test.py
+++ tests/tk_smoke_test.py
@@ -6,17 +6,22 @@ from abstract_turtle import Turtle
 from abstract_turtle.tk_canvas import TkCanvas
 from .image_tests import PillowTest
 
+MANUAL_INSPECT = False
+
 # unfortunately this can't run in headless mode. Delete this line to visually inspect the results
-@unittest.skip
 class TkSmokeTest(PillowTest):
     def setUp(self, width=1000, height=1000):
         self.canvas = TkCanvas(width, height)
         self.turtle = Turtle(self.canvas)
         self.turtle.clear()
         self.turtle.bgcolor("white")
-        import turtle
+        if not MANUAL_INSPECT:
+            import turtle
+            turtle.speed(0)
 
     def assertImageEquals(self, path, hide_turtle=True):
-        sleep(1) # give a second to inspect the image
+        if MANUAL_INSPECT:
+             # give a second to inspect the image
+            sleep(1)
 
 del PillowTest
